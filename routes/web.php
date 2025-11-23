964f7f69513e88b92579305dd7287d37120a1b50
Nowsin-Akter <nowsinakternuha@gmail.com>
2025-11-23 09:57:25 +0600

    Frontend UI update

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return view('welcome');
 });
+
 Route::get('/blog', [DashboardController::class, 'blog']);
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+Route::get('/blog', function(){
+    return view('frontend.app');
+});
 
 require __DIR__.'/auth.php';
